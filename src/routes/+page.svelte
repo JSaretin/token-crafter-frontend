304116d09efce87d461038d85e51cd6a1949c678
JSaretin <jsaretin@gmail.com>
2024-06-02 14:55:58 +0100

    added social to home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Socials from "$lib/componets/Socials.svelte";
+
+</script>
 <svelte:head>
 	<title>Manage and create tokens</title>
 </svelte:head>
@@ -6,3 +10,8 @@
     <a href="/manage-tokens" class="py-2 px-4 rounded-2xl flex-1 bg-orange-400">Manage Token</a>
     <a href="/create-token" class="py-2 px-4 rounded-2xl flex-1 bg-green-400">Create Token</a>
 </div>
+
+<div class="mt-6">
+    <Socials/>
+</div>
+
